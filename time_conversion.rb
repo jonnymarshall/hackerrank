0d04a7f382bd5a85001425e6f81d0cee1463947b
Jonny Marshall <jonnymarshall5@gmail.com>
2020-01-05 23:02:19 +1100

    passed challenge but code needs refactoring

--- time_conversion.rb
+++ time_conversion.rb
@@ -7,14 +7,29 @@ def timeConversion(s)
     if time_split[0] < "12"
       time_split[0] = (time_split[0].to_i + 12).to_s
     else
-      time_split[0] = "00"
+      time_split[0] = "12"
     end
     time_split.join(":")
   end
-  s.slice(8..9) == "AM" ? s.slice(0..7) : pm_conversion(s.slice(0..7))
+
+  def am_conversion(s)
+    time_split = s.split(/:/)
+    time_split[0] = "00" if time_split[0] == "12"
+    time_split.join(":")
+  end
+
+  s.slice(8..9) == "AM" ? am_conversion(s.slice(0..7)) : pm_conversion(s.slice(0..7))
 end
 
 
-# p timeConversion("12:05:45PM") == "12:05:45"
+p timeConversion("12:00:00PM")
+# == "12:00:00"
 
-p timeConversion("12:05:45PM")
+p timeConversion("12:00:00AM")
+# == "00:00:00"
+
+p timeConversion("11:00:00PM")
+# == "23:00:00"
+
+p timeConversion("11:00:00AM")
+# == "11:00:00"

0fbbf8814bd0cca837365fb8d2370f963b19ed03
Jonny Marshall <jonnymarshall5@gmail.com>
2020-01-05 22:46:19 +1100

    fixed issue converting pm times

--- time_conversion.rb
+++ time_conversion.rb
@@ -1,15 +1,20 @@
 require 'byebug'
 
 def timeConversion(s)
-  arr = s.split(/:/)
-  # byebug
-  arr[0] = arr[0].to_i + 12
-  arr[-1] = arr.last[0..1]
-  arr.join(":")
+  # check if it's am or pm
+  def pm_conversion(s)
+    time_split = s.split(/:/)
+    if time_split[0] < "12"
+      time_split[0] = (time_split[0].to_i + 12).to_s
+    else
+      time_split[0] = "00"
+    end
+    time_split.join(":")
+  end
+  s.slice(8..9) == "AM" ? s.slice(0..7) : pm_conversion(s.slice(0..7))
 end
 
 
-# p timeConversion("07:05:45PM") == "19:05:45"
+# p timeConversion("12:05:45PM") == "12:05:45"
 
-p timeConversion("12:05:45AM")
- # == "00:45:45"
+p timeConversion("12:05:45PM")
